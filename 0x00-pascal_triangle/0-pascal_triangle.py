8f6a10d025a5ebceb0f625c82112fa86c22580df
uwen_godwin <uwengodwin001@gmail.com>
2024-07-01 16:52:29 +0100

    alx interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Module to generate Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing Pascal's triangle of n
@@ -15,14 +16,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    
-    return triangle
 
+    return triangle
