c0a2052f7a5e75e102310a86204df3d379111227
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 16:27:29 +0100

    min operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
-This module contains a function to calculate the minimum number of operations
-needed to achieve exactly n H characters in a text file starting with a single H.
+This module contains a function to calculate the
+minimum number of operations
+needed to achieve exactly n H characters in
+a text file starting with a single H.
 """
+
 
 def minOperations(n):
     """
-    Calculate the fewest number of operations needed to result in exactly n H characters.
+    Calculate the fewest number of
+    operations needed to result in exactly n H characters.
 
     :param n: int, the target number of H characters
     :return: int, the minimum number of operations needed or 0 if impossible
