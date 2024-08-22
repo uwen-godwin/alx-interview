19281ed5dee0ac3bc1252f3d930e62098b052fc3
uwen_godwin <uwengodwin001@gmail.com>
2024-08-22 12:12:10 +0100

    making changes

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -5,6 +5,5 @@ Main file for testing the makeChange function.
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 2, 25], 37))  # Expected output: 7 (25 + 10 + 2)
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Expected output: -1
-
+print(makeChange([1, 2, 25], 37))
+print(makeChange([1256, 54, 48, 16, 102], 1453))
