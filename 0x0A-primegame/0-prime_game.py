ef573ed4aa2101e4c671282f461aeb47c2f87f70
uwen_godwin <uwengodwin001@gmail.com>
2024-09-06 07:53:45 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,54 +1,54 @@
 #!/usr/bin/python3
-
 def sieve_of_eratosthenes(n):
-    """
-    Returns a list of prime numbers up to n using the
-    Sieve of Eratosthenes algorithm.
-    """
+    """Return a list of booleans representing primes up to n using the Sieve of Eratosthenes."""
     sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
+    sieve[0] = sieve[1] = False  # 0 and 1 are not primes
     for i in range(2, int(n ** 0.5) + 1):
         if sieve[i]:
             for j in range(i * i, n + 1, i):
                 sieve[j] = False
-    return [i for i, is_prime in enumerate(sieve) if is_prime]
+    return sieve
+
+
+def play_game(n, primes):
+    """Simulate one round of the game and return the winner ("Maria" or "Ben")."""
+    nums = list(range(1, n + 1))
+    maria_turn = True  # Maria always starts
+    while True:
+        # Find the next prime number in the list
+        prime_found = False
+        for i in range(2, n + 1):
+            if primes[i] and i in nums:
+                prime_found = True
+                # Remove the prime and all its multiples
+                nums = [num for num in nums if num % i != 0]
+                break
+
+        if not prime_found:
+            # No more primes left, current player loses
+            return "Maria" if not maria_turn else "Ben"
+
+        # Alternate turns
+        maria_turn = not maria_turn
 
 
 def isWinner(x, nums):
-    """
-    Determines the winner of a game between Maria and Ben.
-
-    Parameters:
-    - x (int): The number of rounds.
-    - nums (list): Array containing the upper limit n for each round.
-
-    Returns:
-    - The name of the player that won the most rounds ("Maria" or "Ben").
-      If the result is a tie or no game is played, return None.
-    """
-    if not nums or x < 1:
+    """Determine the overall winner after x rounds."""
+    if x < 1 or not nums:
         return None
 
-    # Find the maximum number in nums to calculate primes up to that number
     max_num = max(nums)
     primes = sieve_of_eratosthenes(max_num)
-
+    
     maria_wins = 0
     ben_wins = 0
 
-    # For each round, determine who wins based on the number of primes <= n
     for n in nums:
-        primes_count = 0
-        for prime in primes:
-            if prime <= n:
-                primes_count += 1
-            else:
-                break
-        # Maria wins if the number of primes is odd, Ben wins if it's even
-        if primes_count % 2 == 0:
-            ben_wins += 1
-        else:
+        winner = play_game(n, primes)
+        if winner == "Maria":
             maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
